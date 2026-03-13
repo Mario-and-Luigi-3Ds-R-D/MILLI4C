130aff8185bf9f1af3d3634cfceace1f28ee36dd
LoigiFan72 <stephenakins855@gmail.com>
2026-03-13 14:03:48 -0400

    scriptsys commit in last i forgot to stage oopsie

--- src/game/System/ScriptSystem.cpp
+++ src/game/System/ScriptSystem.cpp
@@ -1,9 +1,6 @@
 #include <System/ScriptSystem.hpp>
 extern const unsigned char unk_658108[];
 
-//extern const unsigned char unk_658108{
-//};
-
 ScriptSystem::ScriptSystem(){
     this->mStart = 0;
 }
@@ -17,37 +14,25 @@ void ScriptSystem::init(const void*){
     return;
 }
 
-//NON-MATCHING
-
-int ScriptSystem::rewind(ScriptUnit* unit, const ScriptCode* code){
-}
-
-// Hack coming up..? *Eyes emoji*
+// No Hacks here, unfortately..
 //
-// Pain in the ass
-/*int ScriptSystem::rewind(ScriptUnit * pCurrentUnit,ScriptCode const* pCurrentCode){
-    int param_4 = this->mStart;
-    uint param_8; // r2
-    int param_r0 = 6;
-    int param_9; // r3
-    uchar* param_6 = (uchar*)param_4 + 16 * pCurrentCode->mCodeNum; // first add, param_6 could be r4
-    int param_7 = *param_6 & 0x7F; //
+// Was a pain in thr ass, users Darxoon, Moddimation, M-1, Troy, and finally our G cuckydev got it to work, Hoo-ray!
+int ScriptSystem::rewind(ScriptUnit* pCurrentUnit, ScriptCode const* pCurrentCode) {
+    unsigned short codeNum = pCurrentCode->mCodeNum;
+    int tmp = 6;
+    uchar *base = ((16 * codeNum) + (uchar*)this->mStart);
 
-    if((*param_6 & 0x80) != 0)
-        ++param_7;
+    int count = *base & 0x7F;
+    if ((*base & 0x80) != 0)
+        count++;
     
-    if(param_7 > 0){
-        param_8 = pCurrentCode->mCodeUnit;
-        param_9 = 0;
-        do{
-            if(((param_8 >> param_9) & 1) != 0)
-                param_9 += 2;
-            else
-                param_r0 += unk_658108[((uint) param_6[param_r0 / 2 + 1] >> ((4 * param_9) & 7)) & 0xF];
-                param_7--;
-                ++param_9;
-        } while(param_7);
+    for (int i = 0; i < count; i++) {
+        if ((pCurrentCode->mCodeUnit >> i) & 1) {
+            tmp += 2;
+        } else {
+            tmp += unk_658108[(base[i / 2 + 1] >> ((i & 1) << 2)) & 0xF];
+        }
     }
-    pCurrentUnit->mUnitValue -= param_r0;
-    return param_r0;
-}*/
+    pCurrentUnit->mUnitValue -= tmp;
+    return tmp;
+}
